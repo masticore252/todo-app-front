cb088bc192bb3876ca4832b2d1df74d36545b3e0
Albert Vásquez <albert.vasquez@bairesdev.com>
2019-09-28 15:42:25 -0500

    add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
+
+
+const routes: Routes = [
+  { path: '', component: TasksListComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { TasksListComponent } from './tasks-list/tasks-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from "@angular/forms";
 import { TaskService } from './tasks-list/task.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { TaskService } from './tasks-list/task.service';
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    AppRoutingModule,
   ],
   providers: [TaskService],
   bootstrap: [AppComponent]
